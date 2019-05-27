1b2e7debe9b9f6216b5c806d4d635ac6c5d01db2
Rafael Garcia <1337technomage@gmail.com>
2019-05-27 10:15:31 -0400

    Initial Commit

---
+++ WREK Exporter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WREK Exporter
+//
+//  Created by David Garcia on 5/27/19.
+//  Copyright © 2019 Ayy Lmao LLC. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
